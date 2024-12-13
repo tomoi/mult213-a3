d44c6cb6f9e0e8dee052cc1703c28434c37dd55c
Tomas Godoybaca <tommygodoybaca@outlook.com>
2024-12-13 10:43:56 -0600

    added static characters to work with before i get the search working - made some progress on the search, figuring out how to get it to return the search values to aprent with the delay

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +6,157 @@ import './App.css'
 const apiKey = "fd5e12522935425197fc964d95235df6";
 const apiUrl = "https://www.bungie.net/Platform/";
 const imgPath = "https://www.bungie.net";
+
+
+let staticCharacters = {
+  "2305843009265520769": {
+    "membershipId": "4611686018443871540",
+    "membershipType": 1,
+    "characterId": "2305843009265520769",
+    "dateLastPlayed": "2024-11-29T19:49:55Z",
+    "minutesPlayedThisSession": "1",
+    "minutesPlayedTotal": "10418",
+    "light": 2020,
+    "stats": {
+      "144602215": 50,
+      "392767087": 100,
+      "1735777505": 91,
+      "1935470627": 2020,
+      "1943323491": 52,
+      "2996146975": 22,
+      "4244567218": 51
+    },
+    "raceHash": 3887404748,
+    "genderHash": 3111576190,
+    "classHash": 3655393761,
+    "raceType": 0,
+    "classType": 0,
+    "genderType": 0,
+    "emblemPath": "/common/destiny2_content/icons/cb105a020b9c5c315c59b2b1df3885d6.jpg",
+    "emblemBackgroundPath": "/common/destiny2_content/icons/724358bfba10e751c7e7dc2e2dd3b204.jpg",
+    "emblemHash": 3953403248,
+    "emblemColor": {
+      "red": 1,
+      "green": 0,
+      "blue": 1,
+      "alpha": 255
+    },
+    "levelProgression": {
+      "progressionHash": 1716568313,
+      "dailyProgress": 0,
+      "dailyLimit": 0,
+      "weeklyProgress": 0,
+      "weeklyLimit": 0,
+      "currentProgress": 0,
+      "level": 50,
+      "levelCap": 50,
+      "stepIndex": 50,
+      "progressToNextLevel": 0,
+      "nextLevelAt": 0
+    },
+    "baseCharacterLevel": 50,
+    "percentToNextLevel": 0,
+    "titleRecordHash": 3464275895
+  },
+  "2305843009265520954": {
+    "membershipId": "4611686018443871540",
+    "membershipType": 1,
+    "characterId": "2305843009265520954",
+    "dateLastPlayed": "2024-11-29T20:32:27Z",
+    "minutesPlayedThisSession": "32",
+    "minutesPlayedTotal": "153095",
+    "light": 2018,
+    "stats": {
+      "144602215": 62,
+      "392767087": 91,
+      "1735777505": 101,
+      "1935470627": 2018,
+      "1943323491": 90,
+      "2996146975": 29,
+      "4244567218": 30
+    },
+    "raceHash": 898834093,
+    "genderHash": 2204441813,
+    "classHash": 2271682572,
+    "raceType": 2,
+    "classType": 2,
+    "genderType": 1,
+    "emblemPath": "/common/destiny2_content/icons/d0d8a24c8a8143747adb192caaa43a5f.jpg",
+    "emblemBackgroundPath": "/common/destiny2_content/icons/0104edbc63c03be30602d9b57d77b8be.jpg",
+    "emblemHash": 383734238,
+    "emblemColor": {
+      "red": 18,
+      "green": 0,
+      "blue": 0,
+      "alpha": 255
+    },
+    "levelProgression": {
+      "progressionHash": 1716568313,
+      "dailyProgress": 0,
+      "dailyLimit": 0,
+      "weeklyProgress": 0,
+      "weeklyLimit": 0,
+      "currentProgress": 0,
+      "level": 50,
+      "levelCap": 50,
+      "stepIndex": 50,
+      "progressToNextLevel": 0,
+      "nextLevelAt": 0
+    },
+    "baseCharacterLevel": 50,
+    "percentToNextLevel": 0,
+    "titleRecordHash": 3464275895
+  },
+  "2305843009289646835": {
+    "membershipId": "4611686018443871540",
+    "membershipType": 1,
+    "characterId": "2305843009289646835",
+    "dateLastPlayed": "2024-11-17T03:33:25Z",
+    "minutesPlayedThisSession": "4",
+    "minutesPlayedTotal": "20676",
+    "light": 2017,
+    "stats": {
+      "144602215": 64,
+      "392767087": 104,
+      "1735777505": 84,
+      "1935470627": 2017,
+      "1943323491": 18,
+      "2996146975": 90,
+      "4244567218": 32
+    },
+    "raceHash": 2803282938,
+    "genderHash": 3111576190,
+    "classHash": 671679327,
+    "raceType": 1,
+    "classType": 1,
+    "genderType": 0,
+    "emblemPath": "/common/destiny2_content/icons/f487163bdfc52e126e86b35e7c4a05b0.jpg",
+    "emblemBackgroundPath": "/common/destiny2_content/icons/f97acbe1ff747a86946f98272d4397c9.jpg",
+    "emblemHash": 908153539,
+    "emblemColor": {
+      "red": 2,
+      "green": 9,
+      "blue": 20,
+      "alpha": 255
+    },
+    "levelProgression": {
+      "progressionHash": 1716568313,
+      "dailyProgress": 0,
+      "dailyLimit": 0,
+      "weeklyProgress": 0,
+      "weeklyLimit": 0,
+      "currentProgress": 0,
+      "level": 50,
+      "levelCap": 50,
+      "stepIndex": 50,
+      "progressToNextLevel": 0,
+      "nextLevelAt": 0
+    },
+    "baseCharacterLevel": 50,
+    "percentToNextLevel": 0,
+    "titleRecordHash": 1089543274
+  }
+}
 
 
 //function to get bungie id from user-input text
@@ -119,18 +270,37 @@ function searchResults(searchArray) {
     console.log(searchArray[i]);
     i += 1;
   }
-
 }
+
+function SearchPrint(props) {
+  // let listOfResults = getBungieId(props.results)
+  useEffect(() => {
+    const searchDelay = setTimeout(() => {
+      console.log(props.results);
+      return (getBungieId(props.results));
+    }, 1500)
+
+    return () => clearTimeout(searchDelay)
+  }, [props.results])
+
+  
+  return (
+    <div>
+      <p>Search Results {searchList}</p>
+    </div>
+  )
+}
+
+
 
 function App() {
   const [bungieName, setBungieName] = useState("");
+  const [search, setSearch] = useState("");
 
   //handels submission of the form, stops from refreshing the page and sets the variable to what is in the input field.
   function handleForm() {
     event.preventDefault();
     setBungieName(bungieName);
-    console.log(bungieName);
-    getBungieId(bungieName);
   }
 
 
@@ -139,9 +309,13 @@ function App() {
     <>
       <form id="bungieIdForm" onSubmit={() => handleForm()}>
         <label for="nameInput">Enter a Bungie Id</label>
-        <input type="text" name="nameInput" id="nameInput" placeholder="ex. name#1234" value={bungieName} onChange={(event) => setBungieName(event.target.value)} required />
+        <input type="text" name="nameInput" id="nameInput" placeholder="ex. name#1234" value={bungieName} onChange={(event) => {
+          setBungieName(event.target.value);
+          setSearch(event.target.value);
+        }} required />
         <button>Search</button>
       </form>
+      <SearchPrint results={search} />
       {/* <div className={props.shouldHide ? 'hidden' : undefined} >
         </div> */}
     </>
